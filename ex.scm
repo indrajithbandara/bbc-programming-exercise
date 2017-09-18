a000486956e359096de830a2d49fe9062ccc2ac4
Ryan Watkins <ryanwatkins54@gmail.com>
2017-09-18 07:50:31 +0200

    [A-01]: BBC Programming Exercise
    Add: Error handling for bad uri
    Add: Better unit-testing, mocks offline http-res object
    Cle: Refactor helpers to bottom of module, remove useless code

--- ex.scm
+++ ex.scm
@@ -2,52 +2,85 @@
              (web uri)
              (web response)
              (json)
+             (gnutls)
              (srfi srfi-19)
              (srfi srfi-64))
 
 (define http-res-stdin
-  (lambda ()
+  (lambda (results)
     (display "Insert web address OR type quit to exit: ") (newline)
     (let* ((stdin ((compose symbol->string read))))
-           (try-http-get-json stdin))))
+      (cond ((eq? stdin "quit") (quit))
+            (else (http-res-stdin (cons (http-get-json stdin) results)))))))
 
-(define try-http-get-json
+(define http-res-arg
   (lambda (x)
-    (cond ((eq? x "quit") (exit)
-      (else (http-get-json x))))))
-
-(define http-res-arg (lambda (x) ((compose http-get string->uri) x)))
+    (catch #t
+      (lambda ()
+        ((compose http-get string->uri) x))
+      (lambda (key . parameters)
+        (cond ((eq? key 'getaddrinfo-error) "bad address info")
+              (else (symbol->string key)))))))
 
 (define http-get-json
   (lambda (url)
-    (let* ((res (http-res-arg url))
-           (res-code (response-code res))
-           (res-length (response-content-length res))
-           (res-date-string (date->string (response-date res)))
-           (res-json-str (scm->json-string `(("Url" ,@url)
-                        ("Status_code" ,@res-code)
-                        ("Content_length" ,@res-length)
-                        ("Date" ,@res-date-string)) #:pretty #t)))
-           (cons res-json-str
-             (cons (json-string->scm res-json-str) (quote ()))))))
+    (let* ((res (http-res-arg url)))
+      (http-res-json url res))))
 
-(define get-json-bbc (lambda () (http-get-json "http://www.bbc.co.uk/iplayer")))
+(define http-res-json
+  (lambda (url res)
+    (let*
+      ((res-code (try-res-prop response-code res))
+      (res-length (try-res-prop response-content-length res))
+      (res-m-date (try-res-prop response-date res))
+      (res-date (cond ((date? res-m-date) (date->string res-m-date)) (else #f)))
+      (res-json-str
+      (cond
+        ((or (eq? #f res-code) (eq? #f res-length) (eq? #f res-date))
+          (scm->json-string `(("Url" ,@url) ("Error" ,@res))))
+        (else (scm->json-string `(("Url" ,@url) ("Status_code" ,@res-code)
+          ("Content_length" ,@res-length) ("Date" ,@res-date)) #:pretty #t)))))
+      (display res-json-str) (newline)
+      (cons res-json-str (cons (json-string->scm res-json-str) (quote ()))))))
+
+(define try-res-prop
+  (lambda (f x)
+    (cond ((response? x) (f x))
+          (else #f))))
+
+(define init
+  (lambda ()
+    ;; Uncomment this and comment lines below for stdin
+    ;; (http-res-stdin '())
+
+    (http-get-json "http://www.bbc.co.uk/iplayer")
+    (http-get-json "https://google.com")
+    (http-get-json "bad://address")
+    (http-get-json "http://www.bbc.co.uk/missing/thing")
+    (http-get-json "http://not.exists.bbc.co.uk/")
+    (http-get-json "https://www.oracle.com/technetwork/java/javase/downloads/index.html")
+    (http-get-json "https://www.pets4home.co.uk/images/articles/1646/large/kitten-emergencies-signs-to-look-out-for-537479947ec1c.jpg")
+    (http-get-json "http://site.mockito.org")))
+
+(init)
 
 (define get-json-pretty (lambda (l) (car l)))
 (define get-json-object (lambda (l) (car (cdr l))))
+(define get-keys-hash (lambda (h) (hash-fold (lambda (k v s) (cons k s)) '() h)))
 
-(define bbc-json-response (get-json-bbc))
-(define bbc-json-pretty (get-json-pretty bbc-json-response))
-(define bbc-json-object (get-json-object bbc-json-response))
+(define mock-success-res-hash-keys
+  (lambda ()
+    (let* ((res (build-response))
+           (keys (get-keys-hash (http-res-json res "test_url"))))
+      (display "Built response in unit test: ") (display res) (newline)
+      (display "Keys: ") (display keys) (newline)
+      keys)))
 
-(display bbc-json-pretty) (newline)
-(display bbc-json-object) (newline)
+(mock-success-res-hash-keys)
 
-(define get-keys-hash
-  (lambda (h) (hash-fold (lambda (k v s) (cons k s)) '() h)))
 
 (test-begin "bbc-programming-exam-tests" 1)
 
 (test-equal "json-success-keys-interface"
   '("Date" "Content_length" "Status_code" "Url")
-  (get-keys-hash bbc-json-object))
+  (mock-success-res-hash-keys))
